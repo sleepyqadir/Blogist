082fa1b8df0f4159a6d020c1cb8130f095d4609f
AQadir64 <a.qadir67@outlook.com>
2020-07-25 11:32:35 +0500

    added : blogist create blog screen

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,24 @@
+import React, { useState, useContext } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { Context } from "../context/BlogContext";
+import Form from "../components/Form";
+
+const CreateScreen = ({ navigation }) => {
+  const { addBlogPost } = useContext(Context);
+  return (
+    <Form
+      onSubmit={(newPost) => {
+        addBlogPost(newPost, () => {
+          navigation.navigate("Home");
+        });
+      }}
+      buttonTitle="Add blog"
+      initialState={{ title: "", content: "" }}
+    />
+  );
+};
+
+export default CreateScreen;
+
+
